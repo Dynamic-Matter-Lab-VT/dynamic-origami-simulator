15bb841167bbc9da16ce8833f6d16b676dcd148e
Yogesh Phalak <yphalak@vt.edu>
2024-06-12 14:06:44 -0400

    [models]: updated for automated experiments

--- src/models/1d_nonliner_spring_mass_damper_chain.py
+++ src/models/1d_nonliner_spring_mass_damper_chain.py
@@ -2,106 +2,98 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import pickle
 import matplotlib.pyplot as plt
+from benchmark_utils import input_func, narma
+import os
+from tqdm import tqdm
 
-n = 10
-ks = np.ones(n) * 2
-ms = np.ones(n) * 1
-bs = np.ones(n) * 0.005
-x0 = np.zeros(n)
-
-
-def f(t, y):
-    x = y[:n]
-    v = y[n:]
-    a = np.zeros(n)
-
-    # |--(k0,b0)--m1--(k1,b1)--m2--(k2,b2)--m3- ... -mn ->
-
-    # spring force + damping force + excitation force
-
-    def f_s(k, dx):
-        return k * dx + 1.0 * dx ** 3
-        # return k * np.sin(dx) / 50
-
-    def f_d(b, dv):
-        return b * dv
-        # return 0
-
-    def f_e(t_):
-        # if t_ < 10:
-        #     # return t_
-        #     return 10.0
-        # else:
-        #     return 0
-        # return 10.0 * np.sin(10 * np.pi * t_)
-        return 100.0
-
-    if n == 1:
-        a[0] = (-f_s(ks[0], x[0])  # spring force
-                - f_d(bs[0], v[0])  # damping force
-                + f_e(t)) / ms[0]  # excitation force
-    else:
-        a[0] = (-f_s(ks[0], x[0]) + f_s(ks[1], x[1] - x[0])  # spring force
-                - f_d(bs[0], v[0]) + f_d(bs[1], v[1] - v[0])  # damping force
-                + f_e(t)) / ms[0]  # excitation force
-
-        # v[0] = 2.0 * np.sin(10 * np.pi * t)
-
-        for i in range(1, n - 1):
-            a[i] = (-f_s(ks[i - 1], x[i] - x[i - 1]) + f_s(ks[i], x[i + 1] - x[i])  # spring force
-                    - f_d(bs[i - 1], v[i] - v[i - 1]) + f_d(bs[i], v[i + 1] - v[i])  # damping force
-                    + 0) / ms[i]
-
-        a[-1] = (-f_s(ks[-2], x[-1] - x[-2])  # spring force
-                 - f_d(bs[-2], v[-1] - v[-2])  # damping force
-                 + 0) / ms[-1]  # excitation force
-
-    return np.concatenate([v, a])
+data_dir = os.path.dirname(
+    os.path.dirname(os.path.realpath(__file__))) + '/../data/nl_spring_mass_damper_raw_data/'
 
 
-def get_fd_curve(sol):
-    m = 0
-    ys = sol.y
-    ts = sol.t
+def experiment(n):
+    ks = np.ones(n) * 1
+    ms = np.ones(n) * 1
+    bs = np.ones(n) * 0.005
+    x0 = np.zeros(n)
 
-    plt.figure()
-    for i in range(len(ts)):
-        a = f(ts[i], ys[:, i])
-        # plt.plot(ys[m, i], a[m + n], 'ko')
-        plt.plot(ys[m, i], 10.0 * np.sin(10 * np.pi * ts[i]), 'ko')
+    def f(t, y):
+        x = y[:n]
+        v = y[n:]
+        a = np.zeros(n)
 
-    plt.xlim(-1, 1)
+        # |--(k0,b0)--m1--(k1,b1)--m2--(k2,b2)--m3- ... -mn ->
 
-    plt.xlabel('Displacement')
-    plt.ylabel('Acceleration')
-    plt.title('Force Displacement Curve')
-    plt.show()
+        # spring force + damping force + excitation force
+
+        def f_s(k, dx):
+            return k * dx
+            # return k * np.sin(dx) / 50
+
+        def f_d(b, dv):
+            return b * dv
+            # return 0
+
+        def f_e(t_):
+            # if t_ < 1:
+            #     # return t_
+            #     return 0.0
+            # else:
+            #     return 0.1
+            # return 10.0 * np.sin(10 * np.pi * t_)
+            # return 100.0
+            return input_func(t_)
+
+        if n == 1:
+            a[0] = (-f_s(ks[0], x[0])  # spring force
+                    - f_d(bs[0], v[0])  # damping force
+                    + f_e(t)) / ms[0]  # excitation force
+        else:
+            a[0] = (-f_s(ks[0], x[0]) + f_s(ks[1], x[1] - x[0])  # spring force
+                    - f_d(bs[0], v[0]) + f_d(bs[1], v[1] - v[0])  # damping force
+                    + f_e(t)) / ms[0]  # excitation force
+
+            # v[0] = 2.0 * np.sin(10 * np.pi * t)
+
+            for i in range(1, n - 1):
+                a[i] = (-f_s(ks[i - 1], x[i] - x[i - 1]) + f_s(ks[i], x[i + 1] - x[i])  # spring force
+                        - f_d(bs[i - 1], v[i] - v[i - 1]) + f_d(bs[i], v[i + 1] - v[i])  # damping force
+                        + 0) / ms[i]
+
+            a[-1] = (-f_s(ks[-2], x[-1] - x[-2])  # spring force
+                     - f_d(bs[-2], v[-1] - v[-2])  # damping force
+                     + 0) / ms[-1]  # excitation force
+
+        return np.concatenate([v, a])
+
+    def save_solution(sol):
+        # make a datastructure to save the solution
+        data = {'t': sol.t, 'y': sol.y, 'n': n, 'ks': ks, 'ms': ms, 'bs': bs, 'ip': input_func(sol.t)}
+        filename = data_dir + 'linear_%d.pkl' % n
+        with open(filename, 'wb') as f:
+            pickle.dump(data, f)
+
+    t = np.linspace(0, 100, 10000)
+    y0 = np.concatenate([x0, np.zeros(n)])
+    solution = solve_ivp(f, (0, t[-1]), y0, t_eval=t)
+    save_solution(solution)
 
 
-def animate(sol):
-    t = sol.t
-    x = sol.y[:n].T
-    x0 = np.linspace(0, 10, n)
-    # draw masses as circles at x0 + x and animate as time progresses
-    # draw springs as lines
+def linear_regression(num_input, data, target):
+    weights = np.zeros(num_input)
+    biases = 0
+    error = 0
 
-    print(x.shape)
+    for i in range(len(data)):
+        y = np.dot(weights, data[i]) + biases
+        error += (y - target[i]) ** 2
+        weights += 0.01 * (target[i] - y) * data[i]
+        biases += 0.01 * (target[i] - y)
 
-    for i in range(len(t)):
-        plt.plot(x0 + x[i] * 10, np.zeros(n), 'ko')
-        plt.plot(x0 + x[i] * 10, np.zeros(n), 'k-')
-        plt.ion()
-        plt.show()
-        # set x limits to 0, 10
-        plt.xlim(-2, 12)
-        plt.pause(0.001)
-        plt.clf()
+    return weights, biases, error
 
 
-def fourier_analysis(sol):
-    t = sol.t
-    x = sol.y[:n].T
-    # find fft of x and plot
+def fourier_analysis(t, y, n):
+    x = y[:n].T
     X = np.fft.fft(x, axis=0)
     X = np.abs(X)
     X = X[0:int(X.shape[0] / 2)]
@@ -113,15 +105,65 @@ def fourier_analysis(sol):
     plt.show()
 
 
-if __name__ == '__main__':
-    t = np.linspace(0, 60, 1000)
-    y0 = np.concatenate([x0, np.zeros(n)])
-    solution = solve_ivp(f, (0, t[-1]), y0, t_eval=t)
+def animate(t, y, n):
+    x = y[:n].T
+    x0 = np.linspace(0, 1, n + 1)
 
-    x = solution.y[:n].T
-    plt.plot(solution.t, x)
+    plt.figure()
+
+    for i in range(len(t)):
+        plt.plot(0, 0, 'r*')
+        plt.plot(np.zeros(2), np.array([0, x0[1] + x[i, 0]]), 'b-')
+        plt.plot(np.zeros(n), x0[1:n + 1] + x[i], 'ko')
+        plt.plot(np.zeros(n), x0[1:n + 1] + x[i], 'b-')
+        plt.text(0.8, 1.0, 't: %.1f' % t[i], fontsize=12)
+        plt.ion()
+        plt.show()
+        plt.ylim(-0.01, 1.2)
+        plt.xlim(-1, 1)
+        plt.pause(0.001)
+        plt.clf()
+
+    plt.show()
+    plt.ioff()
+
+
+def plot(t, y, n):
+    x = y[:n].T
+    plt.figure()
+    for i in range(n):
+        plt.plot(t, x[:, i], c=plt.cm.jet(i / n))
     plt.show()
 
-    # animate(solution)
-    # get_fd_curve(solution)
-    fourier_analysis(solution)
+
+if __name__ == '__main__':
+    num_exp = 100
+    bar = tqdm(total=num_exp)
+    for n in range(1, num_exp + 1):
+        experiment(n)
+        bar.update(1)
+
+    # read solution example
+    with open(data_dir + 'linear_100.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+    t = data['t']
+    y = data['y']
+    n = data['n']
+    ip = data['ip']
+
+    plt.plot(t, ip)
+    plot(t, y, n)
+    # animate(t, y, n)
+    fourier_analysis(t, y, n)
+
+    # t = np.linspace(0, 60, 1000)
+    #
+    # n = 10
+    # alpha = 0.5
+    # beta = 0.02
+    # gamma = 0.1
+    # delta = 0.1
+    # y, x = narma(n, 1000, alpha, beta, gamma, delta)
+    # plt.plot(t, y)
+    # plt.show()
---
+++ src/models/benchmark_utils.py
@@ -0,0 +1,165 @@
+"""
+List of functions:
+    - memory: predict a previous input of a random input
+    - xor: compute the xor on two consecutive previous inputs of a binary input
+    - narma: chaotic time series
+Typical parameters:
+    - sequence_length=1000: length of the time series
+    - memory_delay=1: how many time steps in the past for the current task
+    - random_state=None: possibility of a random seed for reproducible results
+Output:
+    - input_data: input sequence, np array (n_sequence, sequence_length, 1)
+    - y: output sequence, np array (n_sequence, sequence_length, 1)
+"""
+
+import numpy as np
+from sklearn.utils import check_random_state
+from sklearn import preprocessing
+
+
+def roll_and_concat(input_data, roll_num=1):
+    '''
+    :param input_data: the original data that will be rolled by axis1
+    :param roll_num: how many times will be the original data rolled and concatenated with itself
+    '''
+    n_sequence, sequence_len, input_dim = input_data.shape
+    rolled_data = np.zeros((n_sequence, sequence_len, input_dim * roll_num))
+    for i_roll in range(roll_num):
+        rolled_data[:, :, i_roll * input_dim:(i_roll + 1) * input_dim] = np.roll(input_data, -i_roll - 1, axis=1)
+
+    return rolled_data
+
+
+def memory(sequence_length=1000, n_sequence=1, memory_delay=1, random_state=None):
+    '''
+    Simple memory task:
+    Given a time series, can we recall what was the value memory_delay in the past?
+    '''
+    random_state = check_random_state(random_state)
+
+    input_data = random_state.normal(loc=0., scale=1, size=(n_sequence, sequence_length, 1))
+    y = np.roll(input_data, memory_delay, axis=1)
+
+    return input_data, y
+
+
+def xor(sequence_length=1000, n_sequence=1, memory_delay=1, random_state=None):
+    '''
+    XOR time series
+    '''
+    random_state = check_random_state(random_state)
+
+    input_data = random_state.randint(low=0, high=2, size=(n_sequence, sequence_length, 1))
+    shifted_input_data = np.roll(input_data, 1, axis=1)
+    output_data = np.logical_xor(input_data, shifted_input_data)
+    # Additional shift by memory_delay
+    y = np.roll(output_data, memory_delay, axis=1)
+
+    return input_data, y
+
+
+# def narma(sequence_length=1000, n_sequence=1, random_state=None, input_data=None):
+#     '''
+#     NARMA time series, canonical test in Reservoir Computing
+#     '''
+#     random_state = check_random_state(random_state)
+#
+#     if input_data is None:
+#         input_data = random_state.uniform(high=0.5, size=(n_sequence, sequence_length, 1))
+#         print(input_data.shape)
+#     y = np.zeros((n_sequence, sequence_length))
+#
+#     # Recursive NARMA equation for y
+#     for i in range(10, sequence_length):
+#         y[:, i] = 0.3 * y[:, i - 1] + \
+#                   0.05 * y[:, i - 1] * np.prod(y[:, i - 10:i - 1]) + \
+#                   1.5 * input_data[:, i - 10] * input_data[:, i - 1] + 0.1
+#
+#     return np.tanh(input_data), np.tanh(y)
+
+def input_func(t):
+    f1 = 2.11
+    f2 = 3.73
+    f3 = 4.33
+    return 0.2 * np.sin(2 * np.pi * f1 * t) * np.sin(2 * np.pi * f2 * t) * np.sin(2 * np.pi * f3 * t)
+
+
+def narma(n, dim, alpha=0.3, beta=0.04, gamma=1.5, delta=0.01):
+    m = 2 * n + dim
+    t = np.linspace(0, 5, m)
+    input_data = input_func(t)
+
+    x = np.zeros(m)
+
+    for i in range(n + 1, m):
+        x[i] = alpha * x[i - 1]
+        x[i] += beta * np.sum(x[i - 1] * x[i - n:i - 1])
+        x[i] += gamma * input_data[i - n] * input_data[i - 1] + delta
+
+    return x[2 * n:], input_data[2 * n:]
+
+
+def get_kuramoto_sivashinsky_lyap_exp(sequence_length=1000, n_sequence=100, spatial_points=64):
+    '''
+    Lyapunov exponent for the Kuramoto–Sivashinsky equation, u_t + u*u_x + α*u_xx + γ*u_xxxx = 0.
+    The method is adapted and modified from "Physica D 65 (1993) 117-134" paper, also from
+    https://blog.abhranil.net/2014/07/22/calculating-the-lyapunov-exponent-of-a-time-series-with-python-code/
+    '''
+    from oct2py import octave
+    N = spatial_points
+    h = 0.25  # time step length
+    nstp = sequence_length
+    L = 22.
+    dlist = [[] for i in range(sequence_length)]
+    for i0 in range(n_sequence):
+        a0 = np.random.rand(N - 2, 1) / 2  # just some initial condition
+        [_, fdata] = octave.feval('ksfmstp', a0, L, h, nstp, 1, nout=2)
+        [_, input_data0] = octave.feval('ksfm2real', fdata, L, nout=2)
+
+        a1 = a0 + 10 ** (-7)
+        [_, fdata] = octave.feval('ksfmstp', a1, L, h, nstp, 1, nout=2)
+        [_, input_data1] = octave.feval('ksfm2real', fdata, L, nout=2)
+
+        for k in range(sequence_length):
+            dlist[k].append(np.log(np.linalg.norm(input_data0[k, :] - input_data1[k, :])))
+
+    spectrum = np.zeros((sequence_length))
+    for i in range(sequence_length):
+        spectrum[i] = sum(dlist[i]) / len(dlist[i])
+
+    lyap_exp = np.polyfit(range(len(spectrum)), spectrum, 1)[0] / h
+
+    return lyap_exp
+
+
+def data_preprocessing(data, standardize=None, scale_min_max=None):
+    """
+    :param data: the processing data
+    :param standardise: true or false
+    :param scale: None or a list of lower and higher bowndaries of scaling
+    :return: the processed data
+    """
+    if standardize:
+        for i in range(data.shape[0]):
+            preprocessing.scale(data[i, :, :], axis=0, copy=False)
+    if scale_min_max:
+        data = scale(data, scale_min_max)
+
+    return data
+
+
+def scale(array, min_max, in_place=False):
+    if in_place:
+        a = array.min(axis=(-1, -2))
+        b = array.max(axis=(-1, -2))
+        array = np.transpose(array, axes=(1, 2, 0))
+        array -= a
+        array /= b - a
+        array *= min_max[1] - min_max[0]
+        array += min_max[0]
+        array = np.transpose(array, axes=(2, 0, 1))
+
+    else:
+        a = array.min(axis=(-1, -2))
+        b = array.max(axis=(-1, -2))
+        return (min_max[0] + (min_max[1] - min_max[0]) * (array.T - a) / (b - a)).T
